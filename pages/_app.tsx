8fed38f6fc27b7312abc0120bf97aa3e015ef482
JoaoTony <joaojosetony2016@gmail.com>
2022-03-19 00:47:02 +0100

    feat: first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => (<h1>Hello Worlds</h1>);
+
+export default Home;
