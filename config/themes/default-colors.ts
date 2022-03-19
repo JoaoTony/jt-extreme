6e9de2123f762bc3a23feaef03332446c459c8f1
JoaoTony <joaojosetony2016@gmail.com>
2022-03-19 01:04:09 +0100

    feat: add default colors theme

---
+++ config/themes/default-colors.ts
@@ -0,0 +1,5 @@
+export const DEFUALT_COLORS = {
+  black01: '#000000',
+  white01: '#FFFF',
+  pink01: '#f59',
+}
--- views/landing-page/landing-page.styles.ts
+++ views/landing-page/landing-page.styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+import { DEFUALT_COLORS } from "../../config/themes/default-colors";
 
 export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
-  background-color: #33f;
+  background-color: ${DEFUALT_COLORS.white01};
 `
